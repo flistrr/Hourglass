e922188e726903967bda5d2d695d635734c01dbc
flistrr <flistrrr@gmail.com>
2026-03-15 19:18:59 +0200

    finally no bugs!!!

--- src/main.cpp
+++ src/main.cpp
@@ -3,255 +3,475 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-// --- НАЛАШТУВАННЯ ДИСПЛЕЯ ---
 #define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64 
-#define OLED_ADDR 0x3C 
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-// --- НАЛАШТУВАННЯ ПІНІВ ---
 const int DIN_PIN = 12;
 const int CLK_PIN = 11;
-const int CS_PIN  = 10;
+const int CS_PIN = 10;
 const int BATTERY_PIN = A0;
 
-// --- НАЛАШТУВАННЯ СИСТЕМИ ---
-const int NUM_DEVICES = 2; 
+const int NUM_DEVICES = 2;
 const int MPU_ADDR = 0x68;
 LedControl lc = LedControl(DIN_PIN, CLK_PIN, CS_PIN, NUM_DEVICES);
 
-// --- ЗМІННІ ПІСКУ ---
 byte topGrid[8][8] = {0};
 byte botGrid[8][8] = {0};
-const int ROTATION_TOP = 2; 
-const int ROTATION_BOT = 2; 
+const int ROTATION_TOP = 2;
+const int ROTATION_BOT = 2;
 
-// --- ТАЙМЕРИ ---
 unsigned long lastPhysics = 0;
 unsigned long lastDrop = 0;
 unsigned long lastEqualize = 0;
 unsigned long lastOledUpdate = 0;
 
-// --- НАЛАШТУВАННЯ ЧАСУ ---
-const unsigned long TARGET_TIME_MS = 60000; // 1 хвилина
-unsigned long dropInterval = 1000; 
+const unsigned long TARGET_TIME_MS = 60000;
+unsigned long dropInterval = 1000;
 
-// --- СТАН ---
-int orientation = 0; 
+int orientation = 0;
 int totalSandParticles = 0;
 
-// --- ПРОТОТИПИ ФУНКЦІЙ ---
-void updatePhysicsDownLeft(byte grid[8][8]); 
-void updatePhysicsUpRight(byte grid[8][8]); 
+void updatePhysicsDownLeft(byte grid[8][8]);
+void updatePhysicsUpRight(byte grid[8][8]);
 void updatePhysicsSideways(byte grid[8][8]);
+void updatePhysicsSidewaysNegative(byte grid[8][8]);
 void drawGrid(int index, byte grid[8][8]);
-void setLedRotated(int addr, int row, int col, byte state); 
+void setLedRotated(int addr, int row, int col, byte state);
 void attemptEqualize();
 void readMPU(int16_t &ax, int16_t &ay, int16_t &az);
 int countSand(byte grid[8][8]);
 void updateOLED();
 int getBatteryPercentage();
+void initMatrixes();
+void initMPU();
+void initOLED();
+void initHOURGLASS();
+void checkOrientation(int16_t ax, int16_t ay, int16_t az);
+void handleUpdate(unsigned long currentMillis);
 
-void setup() {
+
+void setup()
+{
   Serial.begin(115200);
-  
-  // Ініціалізація матриць
-  for(int i=0; i<NUM_DEVICES; i++) {
-    lc.shutdown(i, false);
-    lc.setIntensity(i, 2);
-    lc.clearDisplay(i);
-  }
-  
-  // Ініціалізація MPU6050
-  Wire.begin();
-  Wire.beginTransmission(MPU_ADDR); Wire.write(0x6B); Wire.write(0); Wire.endTransmission(true);
-
-  // Ініціалізація OLED
-  if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) { 
-    Serial.println(F("SSD1306 failed"));
-  }
-  display.clearDisplay();
-
-  // Початкове заповнення піском (верхня частина)
-  for(int x=0; x<8; x++) for(int y=0; y<8; y++) topGrid[x][y] = 1;
-  // Трохи порожнечі для старту (щоб пісок почав сипатися красиво)
-  topGrid[7][0] = 0;
-
-  totalSandParticles = countSand(topGrid) + countSand(botGrid);
-  if (totalSandParticles > 0) dropInterval = TARGET_TIME_MS / totalSandParticles;
+  initMatrixes();
+  initMPU();
+  initOLED();
+  initHOURGLASS();
 }
 
-void loop() {
+void loop()
+{
   unsigned long currentMillis = millis();
 
-  // 1. MPU ТА ОРІЄНТАЦІЯ
   int16_t ax, ay, az;
   readMPU(ax, ay, az);
 
-  if (ay < -6000) orientation = 1;       
-  else if (ay > 6000) orientation = 0;   
-  else if (abs(ax) > 6000) orientation = 2; 
+  checkOrientation(ax, ay, az);
 
-  // 2. ФІЗИКА (рух піску всередині матриць)
-  if (currentMillis - lastPhysics > 40) {
-    if (orientation == 0) { updatePhysicsDownLeft(topGrid); updatePhysicsDownLeft(botGrid); }
-    else if (orientation == 1) { updatePhysicsUpRight(topGrid); updatePhysicsUpRight(botGrid); }
-    else if (orientation == 2) { updatePhysicsSideways(topGrid); updatePhysicsSideways(botGrid); }
-    
-    drawGrid(0, topGrid);
-    drawGrid(1, botGrid);
-    lastPhysics = currentMillis;
-  }
-
-  // 3. ТАЙМЕР ПАДІННЯ (переміщення піску між матрицями)
-  if (currentMillis - lastDrop > dropInterval) {
-    bool sandMoved = false;
-    // Падіння вниз
-    if (orientation == 0 && topGrid[7][0] == 1 && botGrid[0][7] == 0) {
-        topGrid[7][0] = 0; botGrid[0][7] = 1; sandMoved = true;
-    }
-    // Падіння вгору (перевертання годинника)
-    else if (orientation == 1 && botGrid[0][7] == 1 && topGrid[7][0] == 0) {
-        botGrid[0][7] = 0; topGrid[7][0] = 1; sandMoved = true;
-    }
-    
-    if (sandMoved) {
-       lastDrop = currentMillis;
-    }
-  }
-
-  // 4. ЕКВАЛАЙЗЕР (вирівнювання рівнів піску при горизонтальному положенні)
-  if (orientation == 2 && (currentMillis - lastEqualize > 400)) {
-    attemptEqualize();
-    lastEqualize = currentMillis;
-  }
-
-  // 5. ОНОВЛЕННЯ ЕКРАНУ OLED
-  if (currentMillis - lastOledUpdate > 500) {
-    updateOLED();
-    lastOledUpdate = currentMillis;
-  }
+  handleUpdate(currentMillis);
 }
 
-// --- ДОПОМІЖНІ ФУНКЦІЇ ---
-
-void updateOLED() {
+void updateOLED()
+{
   display.clearDisplay();
-  
-  int sandSource = (orientation == 0) ? countSand(topGrid) : 
-                   (orientation == 1) ? countSand(botGrid) : 0;
-  
+
+  // Rotate display 180 degrees when hourglass is on top (orientation == 0)
+  if (orientation == 0)
+    display.setRotation(2);  // 180 degrees
+  else
+    display.setRotation(0);  // No rotation
+
+  int sandSource = (orientation == 0) ? countSand(topGrid) : (orientation == 1) ? countSand(botGrid)
+                                                                                : 0;
+
   unsigned long remainingTimeMS = sandSource * dropInterval;
   int seconds = (remainingTimeMS / 1000) % 60;
   int minutes = (remainingTimeMS / 60000);
 
   display.setTextColor(SSD1306_WHITE);
   display.setTextSize(2);
-  display.setCursor(32, 25); 
-  
-  if (orientation == 2) {
+  display.setCursor(32, 25);
+
+  if (orientation == 2 || orientation == 3)
+  {
     display.print("--:--");
-  } else {
-    if (minutes < 10) display.print("0");
+  }
+  else
+  {
+    if (minutes < 10)
+      display.print("0");
     display.print(minutes);
     display.print(":");
-    if (seconds < 10) display.print("0");
+    if (seconds < 10)
+      display.print("0");
     display.print(seconds);
   }
-
-  // Батарея
-  int batPercent = getBatteryPercentage();
-  display.setTextSize(1);
-  display.setCursor(95, 0);
-  display.print(batPercent); display.print("%");
-  
-  display.drawRect(95, 10, 30, 6, SSD1306_WHITE);
-  int fill = map(batPercent, 0, 100, 0, 28);
-  display.fillRect(96, 11, fill, 4, SSD1306_WHITE);
-
   display.display();
 }
 
-int getBatteryPercentage() {
+int getBatteryPercentage()
+{
   int raw = analogRead(BATTERY_PIN);
-  // Калібрування (налаштуй під свій дільник напруги):
-  // 3.0V = ~614, 4.2V = ~860 (при живленні 5V)
   int percent = map(raw, 614, 860, 0, 100);
-  Serial.print("Battery: "); Serial.print(percent); Serial.println("%");
+  Serial.print("Battery: ");
+  Serial.print(percent);
+  Serial.println("%");
   return constrain(percent, 0, 100);
 }
 
-void drawGrid(int addr, byte grid[8][8]) {
-  for (int x = 0; x < 8; x++) for (int y = 0; y < 8; y++) setLedRotated(addr, x, y, grid[x][y]);
+void drawGrid(int addr, byte grid[8][8])
+{
+  for (int x = 0; x < 8; x++)
+    for (int y = 0; y < 8; y++)
+      setLedRotated(addr, x, y, grid[x][y]);
 }
 
-void setLedRotated(int addr, int x, int y, byte state) {
+void setLedRotated(int addr, int x, int y, byte state)
+{
   int rotation = (addr == 0) ? ROTATION_TOP : ROTATION_BOT;
   int nx = x, ny = y;
-  switch (rotation) {
-    case 1: nx = y; ny = 7 - x; break;
-    case 2: nx = 7 - x; ny = 7 - y; break;
-    case 3: nx = 7 - y; ny = x; break;
+  switch (rotation)
+  {
+  case 1:
+    nx = y;
+    ny = 7 - x;
+    break;
+  case 2:
+    nx = 7 - x;
+    ny = 7 - y;
+    break;
+  case 3:
+    nx = 7 - y;
+    ny = x;
+    break;
   }
   lc.setLed(addr, nx, ny, state);
 }
 
-// Фізика піску
-void updatePhysicsDownLeft(byte grid[8][8]) {
-  for (int x = 7; x >= 0; x--) for (int y = 0; y < 8; y++) {
-    if (grid[x][y] == 1) {
-      if (x < 7 && y > 0 && grid[x+1][y-1] == 0) { grid[x][y]=0; grid[x+1][y-1]=1; }
-      else if (x < 7 && grid[x+1][y] == 0) { grid[x][y]=0; grid[x+1][y]=1; }
-      else if (y > 0 && grid[x][y-1] == 0) { grid[x][y]=0; grid[x][y-1]=1; }
+void updatePhysicsDownLeft(byte grid[8][8])
+{
+  for (int x = 7; x >= 0; x--)
+    for (int y = 0; y < 8; y++)
+    {
+      if (grid[x][y] == 1)
+      {
+        if (x < 7 && y > 0 && grid[x + 1][y - 1] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x + 1][y - 1] = 1;
+        }
+        else if (x < 7 && grid[x + 1][y] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x + 1][y] = 1;
+        }
+        else if (y > 0 && grid[x][y - 1] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x][y - 1] = 1;
+        }
+      }
     }
-  }
 }
 
-void updatePhysicsUpRight(byte grid[8][8]) {
-  for (int x = 0; x < 8; x++) for (int y = 7; y >= 0; y--) {
-    if (grid[x][y] == 1) {
-      if (x > 0 && y < 7 && grid[x-1][y+1] == 0) { grid[x][y]=0; grid[x-1][y+1]=1; }
-      else if (x > 0 && grid[x-1][y] == 0) { grid[x][y]=0; grid[x-1][y]=1; }
-      else if (y < 7 && grid[x][y+1] == 0) { grid[x][y]=0; grid[x][y+1]=1; }
+void updatePhysicsUpRight(byte grid[8][8])
+{
+  for (int x = 0; x < 8; x++)
+    for (int y = 7; y >= 0; y--)
+    {
+      if (grid[x][y] == 1)
+      {
+        if (x > 0 && y < 7 && grid[x - 1][y + 1] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x - 1][y + 1] = 1;
+        }
+        else if (x > 0 && grid[x - 1][y] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x - 1][y] = 1;
+        }
+        else if (y < 7 && grid[x][y + 1] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x][y + 1] = 1;
+        }
+      }
     }
-  }
 }
 
-void updatePhysicsSideways(byte grid[8][8]) {
-  for (int y = 7; y >= 0; y--) for (int x = 0; x < 8; x++) {
-    if (grid[x][y] == 1) {
-      if (y < 7 && grid[x][y+1] == 0) { grid[x][y]=0; grid[x][y+1]=1; }
-      else if (y < 7 && x < 7 && grid[x+1][y+1] == 0) { grid[x][y]=0; grid[x+1][y+1]=1; }
-      else if (y < 7 && x > 0 && grid[x-1][y+1] == 0) { grid[x][y]=0; grid[x-1][y+1]=1; }
+void updatePhysicsSideways(byte grid[8][8])
+{
+  // yPlus: positive X is down, sand falls toward positive X and downward
+  // BUT sand should spread on BOTH sides
+  for (int y = 7; y >= 0; y--)
+    for (int x = 7; x >= 0; x--)
+    {
+      if (grid[x][y] == 1)
+      {
+        // Primary: fall straight down
+        if (y < 7 && grid[x][y + 1] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x][y + 1] = 1;
+        }
+        // Secondary: diagonal down-right (gravity direction +x)
+        else if (y < 7 && x < 7 && grid[x + 1][y + 1] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x + 1][y + 1] = 1;
+        }
+        // Tertiary: diagonal down-left (opposite direction)
+        else if (y < 7 && x > 0 && grid[x - 1][y + 1] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x - 1][y + 1] = 1;
+        }
+        // Quaternary: move right (spread towards gravity)
+        else if (x < 7 && grid[x + 1][y] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x + 1][y] = 1;
+        }
+        // Quinary: move left (spread away from gravity)
+        else if (x > 0 && grid[x - 1][y] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x - 1][y] = 1;
+        }
+      }
     }
-  }
 }
 
-void attemptEqualize() {
+void updatePhysicsSidewaysNegative(byte grid[8][8])
+{
+  // Змінюємо напрямок циклу y: тепер низ знаходиться біля y = 0
+  for (int y = 0; y < 8; y++)
+    for (int x = 0; x < 8; x++)
+    {
+      if (grid[x][y] == 1)
+      {
+        // Primary: fall straight down (тепер це y - 1)
+        if (y > 0 && grid[x][y - 1] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x][y - 1] = 1;
+        }
+        // Secondary: diagonal down-left (gravity direction -x)
+        else if (y > 0 && x > 0 && grid[x - 1][y - 1] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x - 1][y - 1] = 1;
+        }
+        // Tertiary: diagonal down-right (opposite direction)
+        else if (y > 0 && x < 7 && grid[x + 1][y - 1] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x + 1][y - 1] = 1;
+        }
+        // Quaternary: move left (spread towards gravity)
+        else if (x > 0 && grid[x - 1][y] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x - 1][y] = 1;
+        }
+        // Quinary: move right (spread away from gravity)
+        else if (x < 7 && grid[x + 1][y] == 0)
+        {
+          grid[x][y] = 0;
+          grid[x + 1][y] = 1;
+        }
+      }
+    }
+}
+
+void attemptEqualize()
+{
   int cTop = countSand(topGrid);
   int cBot = countSand(botGrid);
-  if (abs(cTop - cBot) <= 2) return;
-  if (cTop > cBot) {
-    if (botGrid[0][7] == 1) return; 
+
+  if (abs(cTop - cBot) <= 2)
+    return;
+
+  if (cTop > cBot)
+  {
+    // Top has more sand, move from top to bottom
+    if (botGrid[0][7] == 1)
+      return;  // Destination occupied
+
+    // Find first sand particle in topGrid and remove it
     bool found = false;
-    for(int x=0; x<8; x++) for(int y=0; y<8; y++) if(topGrid[x][y] == 1) { topGrid[x][y]=0; found=true; break; }
-    if(found) botGrid[0][7] = 1;
-  } else {
-    if (topGrid[7][0] == 1) return;
+    for (int x = 0; x < 8 && !found; x++)
+    {
+      for (int y = 0; y < 8 && !found; y++)
+      {
+        if (topGrid[x][y] == 1)
+        {
+          topGrid[x][y] = 0;
+          botGrid[0][7] = 1;
+          found = true;
+        }
+      }
+    }
+  }
+  else
+  {
+    // Bottom has more sand, move from bottom to top
+    if (topGrid[7][0] == 1)
+      return;  // Destination occupied
+
+    // Find first sand particle in botGrid and remove it
     bool found = false;
-    for(int x=0; x<8; x++) for(int y=0; y<8; y++) if(botGrid[x][y] == 1) { botGrid[x][y]=0; found=true; break; }
-    if(found) topGrid[7][0] = 1;
+    for (int x = 0; x < 8 && !found; x++)
+    {
+      for (int y = 0; y < 8 && !found; y++)
+      {
+        if (botGrid[x][y] == 1)
+        {
+          botGrid[x][y] = 0;
+          topGrid[7][0] = 1;
+          found = true;
+        }
+      }
+    }
   }
 }
 
-int countSand(byte grid[8][8]) {
+int countSand(byte grid[8][8])
+{
   int c = 0;
-  for(int i=0; i<64; i++) if(grid[i/8][i%8]) c++;
+  for (int i = 0; i < 64; i++)
+    if (grid[i / 8][i % 8])
+      c++;
   return c;
 }
 
-void readMPU(int16_t &ax, int16_t &ay, int16_t &az) {
-  Wire.beginTransmission(MPU_ADDR); Wire.write(0x3B); Wire.endTransmission(false);
-  Wire.requestFrom(MPU_ADDR, 6, true); 
-  ax = Wire.read()<<8|Wire.read(); ay = Wire.read()<<8|Wire.read(); az = Wire.read()<<8|Wire.read();  
+void readMPU(int16_t &ax, int16_t &ay, int16_t &az)
+{
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 6, true);
+  ax = Wire.read() << 8 | Wire.read();
+  ay = Wire.read() << 8 | Wire.read();
+  az = Wire.read() << 8 | Wire.read();
+}
+
+void initMatrixes()
+{
+  for (int i = 0; i < NUM_DEVICES; i++)
+  {
+    lc.shutdown(i, false);
+    lc.setIntensity(i, 2);
+    lc.clearDisplay(i);
+  }
+}
+
+void initMPU()
+{
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+}
+
+void initOLED()
+{
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR))
+  {
+    Serial.println(F("SSD1306 failed"));
+  }
+  display.clearDisplay();
+}
+
+void initHOURGLASS()
+{
+  for (int x = 0; x < 8; x++)
+    for (int y = 0; y < 8; y++)
+      topGrid[x][y] = 1;
+  topGrid[7][0] = 0;
+
+  totalSandParticles = countSand(topGrid) + countSand(botGrid);
+  if (totalSandParticles > 0)
+    dropInterval = TARGET_TIME_MS / totalSandParticles;
+}
+
+void checkOrientation(int16_t ax, int16_t ay, int16_t az)
+{
+  if (ay < -6000)
+    orientation = 1;
+  else if (ay > 6000)
+    orientation = 0;
+  else if (ax > 6000)
+    orientation = 2;  // yPlus
+  else if (ax < -6000)
+    orientation = 3;  // yMinus
+}
+
+void handleUpdate(unsigned long currentMillis)
+{
+  if (currentMillis - lastPhysics > 40)
+  {
+    if (orientation == 0)
+    {
+      updatePhysicsDownLeft(topGrid);
+      updatePhysicsDownLeft(botGrid);
+    }
+    else if (orientation == 1)
+    {
+      updatePhysicsUpRight(topGrid);
+      updatePhysicsUpRight(botGrid);
+    }
+    else if (orientation == 2)
+    {
+      updatePhysicsSideways(topGrid);
+      updatePhysicsSideways(botGrid);
+    }
+    else if (orientation == 3)
+    {
+      updatePhysicsSidewaysNegative(topGrid);
+      updatePhysicsSidewaysNegative(botGrid);
+    }
+
+    drawGrid(0, topGrid);
+    drawGrid(1, botGrid);
+    lastPhysics = currentMillis;
+  }
+
+  if (currentMillis - lastDrop > dropInterval)
+  {
+    bool sandMoved = false;
+    if (orientation == 0 && topGrid[7][0] == 1 && botGrid[0][7] == 0)
+    {
+      topGrid[7][0] = 0;
+      botGrid[0][7] = 1;
+      sandMoved = true;
+    }
+    else if (orientation == 1 && botGrid[0][7] == 1 && topGrid[7][0] == 0)
+    {
+      botGrid[0][7] = 0;
+      topGrid[7][0] = 1;
+      sandMoved = true;
+    }
+
+    if (sandMoved)
+    {
+      lastDrop = currentMillis;
+    }
+  }
+
+  if ((orientation == 2 || orientation == 3) && (currentMillis - lastEqualize > 400))
+  {
+    attemptEqualize();
+    lastEqualize = currentMillis;
+  }
+
+  if (currentMillis - lastOledUpdate > 500)
+  {
+    updateOLED();
+    lastOledUpdate = currentMillis;
+  }
 }
