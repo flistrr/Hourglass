c70d94195acac18d794d5ca289276082ca60d14c
flistrrr <flistrrr@gmail.com>
2026-03-24 12:32:02 +0200

    first commit

---
+++ include/MPU.h
@@ -0,0 +1,37 @@
+#include <Wire.h>
+
+class MPU {
+private:
+  int MPU_ADDR;
+
+public:
+  MPU(int mpu);
+  ~MPU();
+  void init();
+  void update();
+  void getOrientation(int16_t &ax, int16_t &ay, int16_t &az);
+};
+
+MPU::MPU(int mpu) : MPU_ADDR(mpu) {}
+
+MPU::~MPU() {}
+
+void MPU::init() {
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+}
+
+void MPU::update() {}
+
+void MPU::getOrientation(int16_t &ax, int16_t &ay, int16_t &az) {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 6, true);
+  ax = Wire.read() << 8 | Wire.read();
+  ay = Wire.read() << 8 | Wire.read();
+  az = Wire.read() << 8 | Wire.read();
+}
